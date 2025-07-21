52b53c70f9f90ae46e5a4ae4f388aefa9b242418
SERHII STETSIUK <skystets@gmail.com>
2025-07-21 18:46:52 +0200

    hw-8

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const userNameInput = document.querySelector('#name-input');
-const userNameElem = document.querySelector('#name-output');
-userNameInput.addEventListener('input', (text) => {
-	userNameInput.value.trim().length != 0 ? userNameElem.textContent = text.currentTarget.value.trim() : userNameElem.textContent = 'Anonymous' ;
-})
